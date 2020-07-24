dcd92971f031a17b6d247502f241a18b31f76cb5
wangzhen <wangzhen@situdata.com>
2020-07-24 22:48:44 +0800

    初步搭建react架子

---
+++ src/layout/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class Layout extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: props.initValue || 0
+    }
+  }
+  render() {
+
+  }
+
+}
