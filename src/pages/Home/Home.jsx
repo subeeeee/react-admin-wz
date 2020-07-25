07193ae10a69230c5c541aac1174a2a8817e6cc6
wangzhen <wangzhen@situdata.com>
2020-07-25 17:05:26 +0800

    解决state问题

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 class Home extends Component{
   constructor(props) {
     super(props)
+    this.state = {}
   }
   render() {
     return(
--- src/pages/Notfound/NotFound.jsx
+++ src/pages/Notfound/NotFound.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 class NotFound extends Component{
   constructor(props) {
     super(props)
+    this.state = {}
   }
   render() {
     return(
